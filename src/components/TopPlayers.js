881fe950bb0e26697193b85493a022b6bb07a09a
IT <itdev1210@gmail.com>
2019-11-28 14:14:52 -0500

    Add fetch methods for getting all players from yesterday's games

--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 
 class TopPlayers extends Component {
-    state = {}
+    constructor() {
+        super()
+        this.state = {
+            players: []
+        }
+    }
+
+    componentDidMount() {
+        let date = new Date();
+        let currentYear = date.getFullYear();
+        let longYesterday = (function (d) { d.setDate(d.getDate() - 1); return d })(new Date())
+        let shortYesterday = longYesterday.toISOString().split('T')[0];
+        fetch(`https://www.balldontlie.io/api/v1/stats?seasons[]=${currentYear}&dates[]=${shortYesterday}&per_page=100`)
+            .then(response => response.json())
+            .then(data => {
+                let currentPage = data.meta.current_page
+                console.log(currentPage)
+                let totalPages = data.meta.total_pages
+                console.log(totalPages)
+                for (let i = currentPage; i <= totalPages; i++) {
+                    fetch(`https://www.balldontlie.io/api/v1/stats?seasons[]=${currentYear}&dates[]=${shortYesterday}&per_page=100&page=` + i)
+                        .then(response => response.json())
+                        .then(data => {
+                            this.setState({
+                                players: this.state.players.concat(data.data)
+                            })
+                        })
+                }
+            })
+    }
+
     render() {
         return (
             <div>
