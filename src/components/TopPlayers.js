61f7a1610429dee9049667b92c24e00ddab0637e
IT <itdev1210@gmail.com>
2019-11-28 13:24:46 -0500

    First commit

---
+++ src/components/TopPlayers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TopPlayers extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TopPlayers;
