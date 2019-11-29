b7bb6fb1ba83f0eff0b89e6322c94ba81f5ee2a5
IT <itdev1210@gmail.com>
2019-11-29 10:31:49 -0500

    Add GameContainer and TopPlayer components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import TopPlayers from './TopPlayers'
+import GameContainer from './GameContainer'
+
 class Main extends Component {
     state = {}
     render() {
         return (
             <div>
-
+                <GameContainer />
+                <TopPlayers />
             </div>
         );
     }
