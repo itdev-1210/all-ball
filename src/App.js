83bfd56ae1c29423f901856e6e1f85e7691b50ec
IT <itdev1210@gmail.com>
2019-11-29 10:18:30 -0500

    Add Main component to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Main from './components/Main'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        <Main />
       </div>
     );
   }
