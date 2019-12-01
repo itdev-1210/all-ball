9ae641cdcebf6d669d91e0e48ce84e258f56df2e
IT <itdev1210@gmail.com>
2019-12-01 13:25:55 -0500

    Display game information through props

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class GameCard extends Component {
-    state = {}
-    render() {
-        return (
-            <div>
-
-            </div>
-        );
-    }
+function GameCard(props) {
+    return (
+        <div>
+            <p>{props.gameData.visitor_team.full_name} {props.gameData.visitor_team_score}</p>
+            <span> @ </span>
+            <p>{props.gameData.home_team.full_name} {props.gameData.home_team_score}</p>
+        </div>
+    );
 }
 
 export default GameCard;
