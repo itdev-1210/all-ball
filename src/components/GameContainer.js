122e2f6d491311f765542c3d9164d96876b533da
IT <itdev1210@gmail.com>
2019-12-01 12:52:09 -0500

    First commit

---
+++ src/components/GameContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class GameContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default GameContainer;
