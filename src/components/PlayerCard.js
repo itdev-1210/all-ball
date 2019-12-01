d7ae87106302c09db2e65aeb09079027dfd54686
IT <itdev1210@gmail.com>
2019-12-01 12:14:50 -0500

    Add player information through props

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-function PlayerCard() {
+
+function PlayerCard(props) {
     return (
-        <div>
+        <div style={{ border: '1px solid black' }}>
+            {props.playerData.player.first_name} {props.playerData.player.last_name}
+            <p>Points: {props.playerData.pts}</p>
+            <p>Assists: {props.playerData.ast}</p>
+            <p>Rebounds: {props.playerData.reb}</p>
+            <p>Steals: {props.playerData.stl}</p>
+            <p>Blocks: {props.playerData.blk}</p>
         </div>
     );
 }
