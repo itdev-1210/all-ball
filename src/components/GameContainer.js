1456e516916d757beb64718f04c7f9673e3f8e30
IT <itdev1210@gmail.com>
2019-12-01 13:27:39 -0500

    Pass game information props to GameCard component

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import GameCard from './GameCard';
+
 class GameContainer extends Component {
     constructor() {
         super()
@@ -31,14 +33,11 @@ class GameContainer extends Component {
     }
 
     render() {
-        const gameInformation = this.state.games.map((team, id) =>
-            <div key={id}>
-                <div>{team.visitor_team.full_name} {team.visitor_team_score}</div>
-                <span> @ </span>
-                <div>{team.home_team.full_name} {team.home_team_score}</div>
-                <br />
-            </div>
-        );
+        const gameInformation = this.state.games.map((gameData, id) =>
+            <GameCard
+                key={id}
+                gameData={gameData}
+            />);
         return (
             <div>
                 {gameInformation}
