a9a65c260e5ad7b39229ae8d8c8f6fd29a4466ea
IT <itdev1210@gmail.com>
2019-12-01 13:04:16 -0500

    First commit

---
+++ src/components/GameCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class GameCard extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default GameCard;
