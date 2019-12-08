59336e3d6ae9e94cde3f007607eb6319f82e6e00
IT <itdev1210@gmail.com>
2019-12-07 23:06:18 -0500

    Add links for home and playersearch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
 class NavBar extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <nav>
+                <ul>
+                    <Link to='/'>
+                        <li>Home</li>
+                    </Link>
+                    <Link to='/playersearch'>
+                        <li>Player Search</li>
+                    </Link>
+                </ul>
+            </nav>
         );
     }
 }
