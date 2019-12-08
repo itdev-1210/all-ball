b2217ad7ee20dc20cac7ee94ad761430e4f07d2c
IT <itdev1210@gmail.com>
2019-12-08 00:10:12 -0500

    Add routing for home page and playersearch page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
+import PlayerSearch from './components/PlayerSearch';
+import NavBar from './components/NavBar';
 import Main from './components/Main'
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   state = {}
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route path='/' exact component={Main} />
+            <Route path='/playersearch' exact component={PlayerSearch} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
