cf646c21a13ff2d99902dc37b0dee4fd73399136
IT <itdev1210@gmail.com>
2019-12-09 20:17:02 -0500

    First commit

---
+++ src/components/PlayerSearch.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PlayerSearch extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div >
+        );
+    }
+}
+
+export default PlayerSearch;
