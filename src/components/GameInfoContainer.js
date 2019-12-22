bfa18043ce3726d871e56a527676815f00200bc2
IT <itdev1210@gmail.com>
2019-12-22 18:26:16 -0500

    First commit

---
+++ src/components/GameInfoContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GameInfoContainer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default GameInfoContainer;
