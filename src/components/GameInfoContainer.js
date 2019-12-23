ac45e5ec9cf6daf4ca4620fd3c242fadf26bc881
IT <itdev1210@gmail.com>
2019-12-22 19:29:56 -0500

    Set state based on which gamecard is clicked

--- src/components/GameInfoContainer.js
+++ src/components/GameInfoContainer.js
@@ -4,8 +4,18 @@ class GameInfoContainer extends React.Component {
     constructor() {
         super()
         this.state = {
-
+            selectedGame: []
         }
+    }
+
+    componentDidMount() {
+        fetch(`https://www.balldontlie.io/api/v1/stats?game_ids[]=${this.props.location && this.props.location.state.game}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    selectedGame: data.data
+                })
+            })
     }
 
     render() {
