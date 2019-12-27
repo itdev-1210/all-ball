06649121f066112b988ab44e774efd55bda16af1
IT <itdev1210@gmail.com>
2019-12-27 10:36:21 -0500

    Import and add route for GameInfoContainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import PlayerSearch from './components/PlayerSearch';
 import NavBar from './components/NavBar';
+import GameInfoContainer from './components/GameInfoContainer';
 import Main from './components/Main'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route path='/' exact component={Main} />
             <Route path='/playersearch' exact component={PlayerSearch} />
+            <Route path='/games/:gameid' exact component={GameInfoContainer} />
           </Switch>
         </div>
       </Router>
