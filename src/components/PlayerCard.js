8a1d88f96edec5cd53441743b5a5326f0f1c23ae
IT <itdev1210@gmail.com>
2020-01-17 19:33:32 -0500

    Add secondary team colour for player card border

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -3,13 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TopFiveContainer = styled.div`
-    border: 3px solid black;
-    border-radius 20px;
+    border-radius 15px;
     flex: 0 1 20%;
     flex-wrap: wrap;
     text-align: center;
     margin-left: 10px;
     margin-right: 10px;
+    color: #FEFEFE;
 `
 
 function PlayerCard(props) {
@@ -20,17 +20,32 @@ function PlayerCard(props) {
         POR: '#E03A3E', SAC: '#5A2D81', SAS: '#C4CED4', TOR: '#CE1141', UTA: '#002B5C', WAS: '#002B5C'
       }
 
-    let teamAbbreviation = props.playerData.team.abbreviation;
-    let playerCardColor = teamHexColors[teamAbbreviation];
+    const teamAbbreviation = props.playerData.team.abbreviation;
+    const playerCardColor = teamHexColors[teamAbbreviation];
+
+    const teamHexSecondColors = {
+        ATL: '#C1D32F', BKN: '#FFFFFF', BOS: '#BA9653', CHA: '#00788C', CHI: '#000000', CLE: '#FDBB30', DAL: '#002B5E', DEN: '#FEC524', 
+        DET: '#1D42BA', GSW: '#FFC72C', HOU: '#000000', IND: '#FDBB30', LAC: '#C8102E', LAL: '#FDB927', MEM: '#12173F', MIA: '#F9A01B', 
+        MIL: '#EEE1C6', MIN: '#236192', NOP: '#0C2340', NYK: '#0072CE', OKC: '#EF3B24', ORL: '#C4CED4', PHI: '#ED174C', PHX: '#E56020', 
+        POR: '#000000', SAC: '#63727A', SAS: '#000000', TOR: '#000000', UTA: '#00471B', WAS: '#E31837'
+      }
+   
+    const playerCardSecondColor = teamHexSecondColors[teamAbbreviation];
 
     return (
-        <TopFiveContainer style={{background: `${playerCardColor}`, color: '#FEFEFE'}}>
-            {props.playerData.player.first_name} {props.playerData.player.last_name}
-            <p>Points: {props.playerData.pts}</p>
-            <p>Assists: {props.playerData.ast}</p>
-            <p>Rebounds: {props.playerData.reb}</p>
-            <p>Steals: {props.playerData.stl}</p>
-            <p>Blocks: {props.playerData.blk}</p>
+        <TopFiveContainer style={{background: `${playerCardColor}`, border: `6px solid ${playerCardSecondColor}`}}>
+            <div>
+                <h2>{props.playerData.player.first_name} {props.playerData.player.last_name} </h2>
+            </div>
+            <h3>Points: {props.playerData.pts}</h3>
+            
+            <h3>Assists: {props.playerData.ast}</h3>
+        
+            <h3>Rebounds: {props.playerData.reb}</h3>
+    
+            <h3>Steals: {props.playerData.stl}</h3>
+        
+            <h3>Blocks: {props.playerData.blk}</h3>
         </TopFiveContainer>
     );
 }
