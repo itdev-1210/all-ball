c007c38281159ac7861e1271225c0b31e7c22cd3
IT <itdev1210@gmail.com>
2020-01-20 21:06:44 -0500

    Style player search results with flexbox

--- src/components/PlayerSearchResults.js
+++ src/components/PlayerSearchResults.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
-function PlayerSearchResults() {
-    return (
-        <div>
+import styled from 'styled-components';
 
-        </div>
+const PlayerInnerContainer = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex: 0 1 20%;
+    justify-content: center;
+    list-style: none;
+    margin: 3px;
+    padding: 20px 10px;
+`
+
+const PlayerFirstName = styled.div`
+`
+
+const PlayerLastName = styled.div`
+    margin-left: 5px;
+`
+
+function PlayerSearchResults(props) {
+    return (
+        <PlayerInnerContainer onClick={props.onClick} style={{flex: '0 1 20%'}}>
+            <PlayerFirstName>{props.players.first_name}</PlayerFirstName>
+             <PlayerLastName>{props.players.last_name}</PlayerLastName>
+        </PlayerInnerContainer>
     );
 }
 
