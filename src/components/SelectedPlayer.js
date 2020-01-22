85916787a34435781adc4cd9de8a37798cfc56ec
IT <itdev1210@gmail.com>
2020-01-21 22:48:03 -0500

    Add basic layout for player bio

--- src/components/SelectedPlayer.js
+++ src/components/SelectedPlayer.js
@@ -1,13 +1,51 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
+const NameContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    font-size: 25px;
+`
+
+const TeamNameContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const FirstName = styled.h1`
+    margin-bottom: 0px;
+    margin-top: 0px;
+    text-align: center;
+`
+
+const LastName = styled.h1`
+    margin-bottom: 0px;
+    margin-left: 13px;
+    margin-top: 0px;
+    text-align: center;
+`
+
+const ProportionContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 function SelectedPlayer(props) {
     return (
-      <div>
-        <h1>{props.player.first_name}</h1>
-        <h1>{props.player.last_name}</h1>
-        <h1>{props.player.team.full_name}</h1>
-        <h2>{props.player.height_feet}'{props.player.height_inches}"</h2>
-        <h2>{props.player.weight_pounds}</h2>
+        <div>
+            <NameContainer>
+                <FirstName>{props.player.first_name} </FirstName>
+                <LastName>{props.player.last_name}</LastName>
+            </NameContainer>
+            <TeamNameContainer>
+                <h2>{props.player.team.full_name}</h2>
+            </TeamNameContainer>
+            <ProportionContainer>
+            <h3>Height: {props.player.height_feet}'{props.player.height_inches}</h3>
+            <h3>Weight: {props.player.weight_pounds}</h3>
+            </ProportionContainer>
         </div>
     );
 }
