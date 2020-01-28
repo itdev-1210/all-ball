7e9ef2cdd89811ffd7981180edd73f0c4cc875ac
IT <itdev1210@gmail.com>
2020-01-27 21:13:10 -0500

    Add table to player season averages

--- src/components/PlayerStats.js
+++ src/components/PlayerStats.js
@@ -1,57 +1,105 @@
-import React from 'react';
+import React from "react";
+
+import styled from "styled-components";
+
+const Table = styled.table`
+  border: 1px solid #ddd;
+  margin: auto;
+  padding; 8px;
+  text-align: left;
+  width: 75%;
+`;
+
+const TableRow = styled.tr`
+	:nth-child(even) {
+		background-color: #f2f2f2;
+	}
+
+	:hover {
+		background-color: #ddd;
+	}
+`;
+
+const TableHeader = styled.th`
+	background-color: lightgray;
+	color: black;
+	padding: 8px;
+	position: sticky;
+	top: 0;
+`;
+
+const TableContainer = styled.div`
+	height: 350px;
+	overflow: auto;
+
+	@media screen and (max-width: 1024px) {
+		margin-left: 17px;
+		margin-right: 17px;
+	}
+`;
+
+const TableData = styled.td``;
 
 function PlayerStats(props) {
-    return (
-        <div style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(21, 1fr)',
-            borderTop: '1px solid black', borderRight: '1px solid black', fontSize: 10
-        }}>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>SEASON</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>GP</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>MIN</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>PTS</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FGA</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FGM</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FGPCT</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FG3M</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FG3A</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FG3PCT</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FTM</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>FTA</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>AST</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>REB</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>DREB</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>OREB</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>AST</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>STL</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>BLK</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>TOV</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>PF</span>
-
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.season}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.games_played}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.min}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.pts}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fga}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fgm}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fg_pct}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fg3m}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fg3a}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fg3_pct}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.ftm}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.fta}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.ast}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.reb}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.dreb}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.oreb}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.ast}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.stl}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.blk}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.turnover}</span>
-            <span style={{ padding: 1, borderLeft: '1px solid black', borderBottom: '1px solid black' }}>{props.seasonStats.pf}</span>
-        </div>
-    );
+	return (
+		<TableContainer>
+			<Table>
+				<thead>
+					<TableRow>
+						<TableHeader>Season</TableHeader>
+						<TableHeader>GP</TableHeader>
+						<TableHeader>MP</TableHeader>
+						<TableHeader>PTS</TableHeader>
+						<TableHeader>FGA</TableHeader>
+						<TableHeader>FGM</TableHeader>
+						<TableHeader>FG%</TableHeader>
+						<TableHeader>FG3A</TableHeader>
+						<TableHeader>FG3M</TableHeader>
+						<TableHeader>FG3%</TableHeader>
+						<TableHeader>FTA</TableHeader>
+						<TableHeader>FTM</TableHeader>
+						<TableHeader>FT%</TableHeader>
+						<TableHeader>REB</TableHeader>
+						<TableHeader>OREB</TableHeader>
+						<TableHeader>DREB</TableHeader>
+						<TableHeader>AST</TableHeader>
+						<TableHeader>STL</TableHeader>
+						<TableHeader>BLK</TableHeader>
+						<TableHeader>TOV</TableHeader>
+						<TableHeader>PF</TableHeader>
+					</TableRow>
+				</thead>
+				<tbody>
+					{props.seasonStats.map((seasonStats, id) => {
+						return (
+							<TableRow key={id}>
+								<TableData>{seasonStats.season}</TableData>
+								<TableData>{seasonStats.games_played}</TableData>
+								<TableData>{seasonStats.min}</TableData>
+								<TableData>{seasonStats.pts}</TableData>
+								<TableData>{seasonStats.fga}</TableData>
+								<TableData>{seasonStats.fgm}</TableData>
+								<TableData>{seasonStats.fg_pct}</TableData>
+								<TableData>{seasonStats.fg3a}</TableData>
+								<TableData>{seasonStats.fg3m}</TableData>
+								<TableData>{seasonStats.fg3_pct}</TableData>
+								<TableData>{seasonStats.fta}</TableData>
+								<TableData>{seasonStats.ftm}</TableData>
+								<TableData>{seasonStats.ft_pct}</TableData>
+								<TableData>{seasonStats.reb}</TableData>
+								<TableData>{seasonStats.oreb}</TableData>
+								<TableData>{seasonStats.dreb}</TableData>
+								<TableData>{seasonStats.ast}</TableData>
+								<TableData>{seasonStats.stl}</TableData>
+								<TableData>{seasonStats.blk}</TableData>
+								<TableData>{seasonStats.turnover}</TableData>
+								<TableData>{seasonStats.pf}</TableData>
+							</TableRow>
+						);
+					})}
+				</tbody>
+			</Table>
+		</TableContainer>
+	);
 }
-
 export default PlayerStats;
