3673fba208d1e8bc3ae9d6123894025da70ede70
IT <itdev1210@gmail.com>
2020-01-29 20:50:03 -0500

    Display table of away teams roster stats from selected game

--- src/components/TeamGameLog.js
+++ src/components/TeamGameLog.js
@@ -45,64 +45,128 @@ class TeamGameLog extends Component {
 		super(props);
 		this.state = {};
 	}
+
 	render() {
+		const homeTeamLog = (
+			<TableContainer>
+				<Table>
+					<thead>
+						<TableRow>
+							<TableHeader>Player</TableHeader>
+							<TableHeader>MP</TableHeader>
+							<TableHeader>PTS</TableHeader>
+							<TableHeader>FGA</TableHeader>
+							<TableHeader>FGM</TableHeader>
+							<TableHeader>FG%</TableHeader>
+							<TableHeader>FG3A</TableHeader>
+							<TableHeader>FG3M</TableHeader>
+							<TableHeader>FG3%</TableHeader>
+							<TableHeader>FTA</TableHeader>
+							<TableHeader>FTM</TableHeader>
+							<TableHeader>FT%</TableHeader>
+							<TableHeader>REB</TableHeader>
+							<TableHeader>OREB</TableHeader>
+							<TableHeader>DREB</TableHeader>
+							<TableHeader>AST</TableHeader>
+							<TableHeader>STL</TableHeader>
+							<TableHeader>BLK</TableHeader>
+						</TableRow>
+					</thead>
+					<tbody>
+						{this.props.homeTeamRoster.map((player, id) => {
+							console.log(player.player);
+							return (
+								<TableRow key={id}>
+									<TableData>
+										{player.player.first_name} {player.player.last_name}
+									</TableData>
+									<TableData>{player.min}</TableData>
+									<TableData>{player.pts}</TableData>
+									<TableData>{player.fga}</TableData>
+									<TableData>{player.fgm}</TableData>
+									<TableData>{player.fg_pct}</TableData>
+									<TableData>{player.fg3a}</TableData>
+									<TableData>{player.fg3m}</TableData>
+									<TableData>{player.fg3_pct}</TableData>
+									<TableData>{player.fta}</TableData>
+									<TableData>{player.ftm}</TableData>
+									<TableData>{player.ft_pct}</TableData>
+									<TableData>{player.reb}</TableData>
+									<TableData>{player.oreb}</TableData>
+									<TableData>{player.dreb}</TableData>
+									<TableData>{player.ast}</TableData>
+									<TableData>{player.stl}</TableData>
+									<TableData>{player.blk}</TableData>
+								</TableRow>
+							);
+						})}
+					</tbody>
+				</Table>
+			</TableContainer>
+		);
+
+		const awayTeamLog = (
+			<TableContainer>
+				<Table>
+					<thead>
+						<TableRow>
+							<TableHeader>Player</TableHeader>
+							<TableHeader>MP</TableHeader>
+							<TableHeader>PTS</TableHeader>
+							<TableHeader>FGA</TableHeader>
+							<TableHeader>FGM</TableHeader>
+							<TableHeader>FG%</TableHeader>
+							<TableHeader>FG3A</TableHeader>
+							<TableHeader>FG3M</TableHeader>
+							<TableHeader>FG3%</TableHeader>
+							<TableHeader>FTA</TableHeader>
+							<TableHeader>FTM</TableHeader>
+							<TableHeader>FT%</TableHeader>
+							<TableHeader>REB</TableHeader>
+							<TableHeader>OREB</TableHeader>
+							<TableHeader>DREB</TableHeader>
+							<TableHeader>AST</TableHeader>
+							<TableHeader>STL</TableHeader>
+							<TableHeader>BLK</TableHeader>
+						</TableRow>
+					</thead>
+					<tbody>
+						{this.props.awayTeamRoster.map((player, id) => {
+							console.log(player.player);
+							return (
+								<TableRow key={id}>
+									<TableData>
+										{player.player.first_name} {player.player.last_name}
+									</TableData>
+									<TableData>{player.min}</TableData>
+									<TableData>{player.pts}</TableData>
+									<TableData>{player.fga}</TableData>
+									<TableData>{player.fgm}</TableData>
+									<TableData>{player.fg_pct}</TableData>
+									<TableData>{player.fg3a}</TableData>
+									<TableData>{player.fg3m}</TableData>
+									<TableData>{player.fg3_pct}</TableData>
+									<TableData>{player.fta}</TableData>
+									<TableData>{player.ftm}</TableData>
+									<TableData>{player.ft_pct}</TableData>
+									<TableData>{player.reb}</TableData>
+									<TableData>{player.oreb}</TableData>
+									<TableData>{player.dreb}</TableData>
+									<TableData>{player.ast}</TableData>
+									<TableData>{player.stl}</TableData>
+									<TableData>{player.blk}</TableData>
+								</TableRow>
+							);
+						})}
+					</tbody>
+				</Table>
+			</TableContainer>
+		);
+
 		return (
 			<div>
-				<TableContainer>
-					<Table>
-						<thead>
-							<TableRow>
-								<TableHeader>Player</TableHeader>
-								<TableHeader>MP</TableHeader>
-								<TableHeader>PTS</TableHeader>
-								<TableHeader>FGA</TableHeader>
-								<TableHeader>FGM</TableHeader>
-								<TableHeader>FG%</TableHeader>
-								<TableHeader>FG3A</TableHeader>
-								<TableHeader>FG3M</TableHeader>
-								<TableHeader>FG3%</TableHeader>
-								<TableHeader>FTA</TableHeader>
-								<TableHeader>FTM</TableHeader>
-								<TableHeader>FT%</TableHeader>
-								<TableHeader>REB</TableHeader>
-								<TableHeader>OREB</TableHeader>
-								<TableHeader>DREB</TableHeader>
-								<TableHeader>AST</TableHeader>
-								<TableHeader>STL</TableHeader>
-								<TableHeader>BLK</TableHeader>
-							</TableRow>
-						</thead>
-						<tbody>
-							{this.props.homeTeamRoster.map((player, id) => {
-								console.log(player.player);
-								return (
-									<TableRow key={id}>
-										<TableData>
-											{player.player.first_name} {player.player.last_name}
-										</TableData>
-										<TableData>{player.min}</TableData>
-										<TableData>{player.pts}</TableData>
-										<TableData>{player.fga}</TableData>
-										<TableData>{player.fgm}</TableData>
-										<TableData>{player.fg_pct}</TableData>
-										<TableData>{player.fg3a}</TableData>
-										<TableData>{player.fg3m}</TableData>
-										<TableData>{player.fg3_pct}</TableData>
-										<TableData>{player.fta}</TableData>
-										<TableData>{player.ftm}</TableData>
-										<TableData>{player.ft_pct}</TableData>
-										<TableData>{player.reb}</TableData>
-										<TableData>{player.oreb}</TableData>
-										<TableData>{player.dreb}</TableData>
-										<TableData>{player.ast}</TableData>
-										<TableData>{player.stl}</TableData>
-										<TableData>{player.blk}</TableData>
-									</TableRow>
-								);
-							})}
-						</tbody>
-					</Table>
-				</TableContainer>
+				{homeTeamLog}
+				{awayTeamLog}
 			</div>
 		);
 	}
