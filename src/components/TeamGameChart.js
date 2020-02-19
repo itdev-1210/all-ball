4c861f808bc413e365902bd518ff6398df3bab7b
IT <itdev1210@gmail.com>
2020-02-18 23:02:46 -0500

    Pass team colors as props to style team abbreviations

--- src/components/TeamGameChart.js
+++ src/components/TeamGameChart.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styled from "styled-components";
+import { teamHexFirstColors, teamHexSecondColors } from "../teamhexcolors";
 
 import {
   ResponsiveContainer,
@@ -15,6 +16,28 @@ import {
 const TeamContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
+`;
+
+const TeamPoints = styled.h1`
+  margin-left: 1.5rem;
+`;
+
+const AwayTeamContainer = styled.div`
+  display: flex;
+`;
+
+const HomeTeamContainer = styled(AwayTeamContainer)``;
+
+const HomeTeamAbbreviation = styled.h1`
+  color: ${props => props.homeTeamColor};
+  -webkit-text-stroke-width: 0.1rem;
+  -webkit-text-stroke-color: ${props => props.homeTeamSecondColor};
+`;
+
+const AwayTeamAbbreviation = styled.h1`
+  color: ${props => props.awayTeamColor};
+  -webkit-text-stroke-width: 0.1rem;
+  -webkit-text-stroke-color: ${props => props.awayTeamSecondColor};
 `;
 
 function TeamGameChart(props) {
@@ -71,74 +94,8 @@ function TeamGameChart(props) {
   const awayTeamTurnovers = awayTeamRoster.reduce((a, b) => a + b.turnover, 0);
   const homeTeamTurnovers = homeTeamRoster.reduce((a, b) => a + b.turnover, 0);
 
-  const teamHexColors = {
-    ATL: "#E03A3E",
-    BKN: "#000000",
-    BOS: "#007A33",
-    CHA: "#1D1160",
-    CHI: "#CE1141",
-    CLE: "#860038",
-    DAL: "#00538C",
-    DEN: "#0E2240",
-    DET: "#C8102E",
-    GSW: "#1D428A",
-    HOU: "#CE1141",
-    IND: "#002D62",
-    LAC: "#1D1160",
-    LAL: "#552583",
-    MEM: "#5D76A9",
-    MIA: "#98002E",
-    MIL: "#00471B",
-    MIN: "#0C2340",
-    NOP: "#85714D",
-    NYK: "#F58426",
-    OKC: "#007AC1",
-    ORL: "#0077C0",
-    PHI: "#006BB6",
-    PHX: "#1D1160",
-    POR: "#E03A3E",
-    SAC: "#5A2D81",
-    SAS: "#C4CED4",
-    TOR: "#CE1141",
-    UTA: "#002B5C",
-    WAS: "#002B5C"
-  };
-
-  const teamHexSecondColors = {
-    ATL: "#C1D32F",
-    BKN: "#FFFFFF",
-    BOS: "#BA9653",
-    CHA: "#00788C",
-    CHI: "#000000",
-    CLE: "#FDBB30",
-    DAL: "#002B5E",
-    DEN: "#FEC524",
-    DET: "#1D42BA",
-    GSW: "#FFC72C",
-    HOU: "#000000",
-    IND: "#FDBB30",
-    LAC: "#C8102E",
-    LAL: "#FDB927",
-    MEM: "#12173F",
-    MIA: "#F9A01B",
-    MIL: "#EEE1C6",
-    MIN: "#236192",
-    NOP: "#0C2340",
-    NYK: "#0072CE",
-    OKC: "#EF3B24",
-    ORL: "#C4CED4",
-    PHI: "#ED174C",
-    PHX: "#E56020",
-    POR: "#000000",
-    SAC: "#63727A",
-    SAS: "#000000",
-    TOR: "#000000",
-    UTA: "#00471B",
-    WAS: "#E31837"
-  };
-
-  const homeTeamColor = teamHexColors[homeTeam];
-  const awayTeamColor = teamHexColors[awayTeam];
+  const homeTeamColor = teamHexFirstColors[homeTeam];
+  const awayTeamColor = teamHexFirstColors[awayTeam];
 
   const homeTeamSecondColor = teamHexSecondColors[homeTeam];
   const awayTeamSecondColor = teamHexSecondColors[awayTeam];
@@ -199,30 +156,24 @@ function TeamGameChart(props) {
   return (
     <div>
       <TeamContainer>
-        <div style={{ display: "flex" }}>
-          <h1
-            style={{
-              color: `${awayTeamColor}`,
-              webkitTextStrokeWidth: "1px",
-              WebkitTextStrokeColor: `${awayTeamSecondColor}`
-            }}
+        <AwayTeamContainer>
+          <AwayTeamAbbreviation
+            awayTeamColor={awayTeamColor}
+            awayTeamSecondColor={awayTeamSecondColor}
           >
             {awayTeam}
-          </h1>
-          <h1 style={{ marginLeft: "15px" }}>{awayTeamPoints}</h1>
-        </div>
-        <div style={{ display: "flex" }}>
-          <h1
-            style={{
-              color: `${homeTeamColor}`,
-              webkitTextStrokeWidth: "1px",
-              WebkitTextStrokeColor: `${homeTeamSecondColor}`
-            }}
+          </AwayTeamAbbreviation>
+          <TeamPoints>{awayTeamPoints}</TeamPoints>
+        </AwayTeamContainer>
+        <HomeTeamContainer>
+          <HomeTeamAbbreviation
+            homeTeamColor={homeTeamColor}
+            homeTeamSecondColor={homeTeamSecondColor}
           >
             {homeTeam}
-          </h1>
-          <h1 style={{ marginLeft: "1.5rem" }}>{homeTeamPoints}</h1>
-        </div>
+          </HomeTeamAbbreviation>
+          <TeamPoints>{homeTeamPoints}</TeamPoints>
+        </HomeTeamContainer>
       </TeamContainer>
       <div style={{ marginLeft: "18%" }}>
         <ResponsiveContainer width="80%" height={500}>
