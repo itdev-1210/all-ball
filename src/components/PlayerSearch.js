758cb5096d4242a61a372b493667b30137ca8373
IT <itdev1210@gmail.com>
2020-03-20 13:41:22 -0400

    Remove unused CSS and pass props to PlayerSearchForm

--- src/components/PlayerSearch.js
+++ src/components/PlayerSearch.js
@@ -4,6 +4,7 @@ import PlayerSearchResults from "./PlayerSearchResults";
 import PlayerStats from "./PlayerStats";
 import PlayerGameLogs from "./PlayerGameLogs";
 import SelectedPlayer from "./SelectedPlayer";
+import PlayerSearchForm from "./PlayerSearchForm";
 import styled from "styled-components";
 
 const PlayerOuterContainer = styled.div`
@@ -13,30 +14,6 @@ const PlayerOuterContainer = styled.div`
   margin: 1.6rem;
   transition: opacity 1s;
   opacity: ${props => (props.isVisible ? "1" : "0")};
-`;
-
-const FormContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 10rem;
-`;
-
-const Form = styled.form`
-  display: flex;
-  justify-content: center;
-  transform: ${props =>
-    props.isClicked ? "translateY(0rem)" : "translateY(12rem)"};
-  transition: transform 0.5s;
-  width: 75%;
-`;
-
-const Input = styled.input`
-  font-size: 16px;
-  border: 1px solid #333;
-  transition: all 0.5s;
-  transform-origin: top left;
-  transform: scale(0.8);
-  width: 50%;
 `;
 
 const GameLog = styled.h3`
@@ -68,33 +45,6 @@ const StatSwitchContainer = styled.div`
 
   @media screen and (min-width: 500px) {
     width: 50%;
-  }
-`;
-
-const SearchButton = styled.button`
-  background-color: #333;
-  border: 0.1rem solid #333
-  color: #fffaf0;
-  font-size: 16px;
-  margin-left: -4rem;
-  padding: 2px;
-  padding-right: 1rem;
-  padding-left: 1rem;
-  transition: all 0.5s;
-  transform-origin: top left;
-  transform: scale(0.8);
-
-  :hover {
-    cursor: pointer;
-    background-color: #5a5a5a;
-  }
-
-  @media screen and (min-width: 600px) {
-    margin-left:-7rem
-  }
-
-  @media screen and (min-width: 1300px) {
-    margin-left:-9rem
   }
 `;
 
@@ -234,22 +184,18 @@ function PlayerSearch() {
       </StatSwitchContainer>
     ) : null;
 
+  const Form = (
+    <PlayerSearchForm
+      handleSearch={handleSearch}
+      isClicked={isClicked}
+      handleChange={handleChange}
+      input={input}
+    />
+  );
+
   return (
     <div>
-      <FormContainer>
-        <Form onSubmit={handleSearch} isClicked={isClicked}>
-          <Input
-            type="text"
-            name="input"
-            value={input}
-            onChange={handleChange}
-            maxLength="25"
-            placeholder="Search for players by name"
-          ></Input>
-          <SearchButton>Search</SearchButton>
-        </Form>
-      </FormContainer>
-
+      {Form}
       <PlayerOuterContainer isVisible={isVisible}>
         {searchedPlayers}
       </PlayerOuterContainer>
