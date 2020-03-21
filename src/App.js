911145cf928d9c100439d4dd76b556be40dfc434
IT <itdev1210@gmail.com>
2020-03-21 15:13:57 -0400

    Add wildcard route for all non specified routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import PlayerSearch from './components/PlayerSearch';
-import NavBar from './components/NavBar';
-import GameInfoContainer from './components/GameInfoContainer';
-import Main from './components/Main'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PlayerSearch from "./components/PlayerSearch";
+import NavBar from "./components/NavBar";
+import GameInfoContainer from "./components/GameInfoContainer";
+import Main from "./components/Main";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-class App extends Component {
-  state = {}
-  render() {
-    return (
-      <Router>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route path='/' exact component={Main} />
-            <Route path='/playersearch' exact component={PlayerSearch} />
-            <Route path='/games/:gameid' exact component={GameInfoContainer} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/playersearch" exact component={PlayerSearch} />
+          <Route path="/games/:gameid" exact component={GameInfoContainer} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
