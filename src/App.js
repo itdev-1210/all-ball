9d7f864194932eb63d5d80085e3a53274cc54c2e
IT <itdev1210@gmail.com>
2020-03-22 19:08:01 -0400

    Add route for Error component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
 import PlayerSearch from "./components/PlayerSearch";
 import NavBar from "./components/NavBar";
 import GameInfoContainer from "./components/GameInfoContainer";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
+import Error from "./components/Error";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" exact component={Main} />
           <Route path="/playersearch" exact component={PlayerSearch} />
           <Route path="/games/:gameid" exact component={GameInfoContainer} />
+          <Route path="/error" exact component={Error} />
           <Route component={NotFound} />
         </Switch>
       </div>
