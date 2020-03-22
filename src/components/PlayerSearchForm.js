bee77143e66f5429b762c292b195edad7eacf3d1
IT <itdev1210@gmail.com>
2020-03-21 21:24:02 -0400

    Update styling for touch devices

--- src/components/PlayerSearchForm.js
+++ src/components/PlayerSearchForm.js
@@ -18,38 +18,48 @@ const Form = styled.form`
 `;
 
 const Input = styled.input`
-  font-size: 16px;
+  font-size: 1.7rem;
   border: 1px solid #333;
-  transition: all 0.5s;
-  transform-origin: top left;
-  transform: scale(0.8);
-  width: 50%;
+  width: 90%;
+
+  @media screen and (min-width: 600px) {
+    width: 50%;
+  }
+
+  @media screen and (min-width: 1500px) {
+    width: 30%;
+  }
+
+  @media (hover: none) {
+    font-size: 16px;
+    transition: all 0.5s;
+    transition: all 0.5s;
+    transform-origin: top left;
+    transform: scale(0.8);
+  }
 `;
 
 const SearchButton = styled.button`
   background-color: #333;
-  border: 0.1rem solid #333
+  border: 0.1rem solid #333;
   color: #fffaf0;
-  font-size: 16px;
-  margin-left: -4rem;
+  font-size: 1.7rem;
   padding: 2px;
   padding-right: 1rem;
   padding-left: 1rem;
   transition: all 0.5s;
-  transform-origin: top left;
-  transform: scale(0.8);
 
   :hover {
     cursor: pointer;
     background-color: #5a5a5a;
   }
 
-  @media screen and (min-width: 600px) {
-    margin-left:-7rem
-  }
-
-  @media screen and (min-width: 1300px) {
-    margin-left:-9rem
+  @media (hover: none) {
+    font-size: 16px;
+    transition: all 0.5s;
+    transform-origin: top left;
+    transform: scale(0.8);
+    margin-left: -5rem;
   }
 `;
 
@@ -57,19 +67,21 @@ function PlayerSearchForm(props) {
   const { handleSearch, isClicked, handleChange, input } = props;
 
   return (
-    <FormContainer>
-      <Form onSubmit={handleSearch} isClicked={isClicked}>
-        <Input
-          type="text"
-          name="input"
-          value={input}
-          onChange={handleChange}
-          maxLength="25"
-          placeholder="Search for players by name"
-        ></Input>
-        <SearchButton>Search</SearchButton>
-      </Form>
-    </FormContainer>
+    <div>
+      <FormContainer>
+        <Form onSubmit={handleSearch} isClicked={isClicked}>
+          <Input
+            type="text"
+            name="input"
+            value={input}
+            onChange={handleChange}
+            maxLength="25"
+            placeholder="Search for players by name"
+          ></Input>
+          <SearchButton>Search</SearchButton>
+        </Form>
+      </FormContainer>
+    </div>
   );
 }
 
